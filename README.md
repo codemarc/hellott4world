0fccc2b4029c77768e97b011741a9918f866bb56
Marc J. Greenberg <marc@Marcs-MacBook-Pro.local>
2019-08-16 19:19:54 -0400

    added some actual generated enum style code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# hellott4world
+# hello-t4-world
 A repo to hold tt4 PoC test code
+
+# steps to package
+
+1. Generate code from template
+    > I have my vscode rigged to run TextTransform.exe  
+    > on my mac I use mono to run it in vscode on save
+
+2. Commit the changes
+    > git commit -m "added some actual generated enum style code"
+
+3. Update the package version with npm
+    > npm version 0.0.3
+
+4. Publish and check
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-﻿module.exports = function() {
-    let msg='Hello T4Template world'
-    console.log(msg)
-    return msg
+﻿
+// Imaging a Javascript Enumeration
+// enum Direction { Up = 1, Down, Left, Right }
+
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up"
+    Direction[Direction["Down"] = 2] = "Down"
+    Direction[Direction["Left"] = 3] = "Left"
+    Direction[Direction["Right"] = 4] = "Right"
+})(Direction || (Direction = {}));
+
+module.exports = {
+    hello: () => 'Hello T4Template world',
+    arrow: (way) => Direction[way]
 }
+
+// Look what I can do...
+// console.log(module.exports.hello())
+// console.log(module.exports.arrow("Up"))
+// console.log(module.exports.arrow("Down"))
+// console.log(module.exports.arrow(3))
+// console.log(module.exports.arrow(4))
